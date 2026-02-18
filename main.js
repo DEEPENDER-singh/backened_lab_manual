faa4a642ec6cd176b207063e74004dbccf93f926
Deeps <your_github_email@gmail.com>
2026-02-18 13:55:07 +0530

    Added Practical 5 Node.js runtime files

---
+++ main.js
@@ -0,0 +1,5 @@
+const utils = require('./utility');
+
+console.log("Addition:", utils.add(10, 5));
+console.log("Subtraction:", utils.subtract(10, 5));
+console.log("Multiplication:", utils.multiply(10, 5));
